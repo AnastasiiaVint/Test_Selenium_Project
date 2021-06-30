b317858747acba4dd46a38416139e40b9442cdf0
AnastasiiaVint <a.vintrovich@gmail.com>
2021-06-30 23:42:21 +0300

    3 commit

--- Tests/Sel_test.py
+++ Tests/Sel_test.py
@@ -1,14 +1,19 @@
+from allure_commons.types import AttachmentType
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 import time
+import allure
 
 class Test_homework():
 
+    @allure.feature('Test')
     def test_first(self):
         driver = webdriver.Chrome()
 
         driver.get("https://google.com")
         time.sleep(4)
+        allure.attach(driver.get_screenshot_as_png(),
+                      name="test_screenshot",
+                      attachment_type=AttachmentType.PNG)
+
         driver.quit()
-
-
