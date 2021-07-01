b166a733d89ebe21537ad7eef607c7be5cc513a1
AnastasiiaVint <a.vintrovich@gmail.com>
2021-07-01 17:14:05 +0300

    7 commit

--- Tests/Sel_test.py
+++ Tests/Sel_test.py
@@ -8,12 +8,12 @@ class Test_homework():
 
     @allure.feature('Test')
     def test_first(self):
-        pass
-        # driver = webdriver.Chrome()
-        #
-        # driver.get("https://google.com")
-        # allure.attach(driver.get_screenshot_as_png(),
-        #               name="test_screenshot",
-        #               attachment_type=AttachmentType.PNG)
-        # time.sleep(4)
-        # driver.quit()
+
+        driver = webdriver.Chrome()
+
+        driver.get("https://google.com")
+        allure.attach(driver.get_screenshot_as_png(),
+                      name="test_screenshot",
+                      attachment_type=AttachmentType.PNG)
+        time.sleep(4)
+        driver.quit()
