4b820e14cc100bd18581868275f2e4a6f2b4889e
AnastasiiaVint <a.vintrovich@gmail.com>
2021-09-30 21:13:51 +0300

    ава

--- Tests/Sel_test.py
+++ Tests/Sel_test.py
@@ -6,7 +6,7 @@ import allure
 
 class Test_homework():
 
-    @allure.feature('Test edit')
+    @allure.feature('Test edit 2')
     def test_first(self):
         pass
         # driver = webdriver.Chrome()
